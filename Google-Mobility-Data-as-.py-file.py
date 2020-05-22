f853d5f80367ba1e7b9109845e76d1749d33a4bb
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-05-21 23:02:50 -0400

    updated 5/21 @ 23:03

--- Google-Mobility-Data-as-.py-file.py
+++ Google-Mobility-Data-as-.py-file.py
@@ -1,10 +1,27 @@
-import os
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+# %%
 file = "Google_Mobility_Report.csv"
 
-df = pd.DataFrame() #establishes df as DataFrame (data table)
-
-pd.read_csv("Google_Mobility_Report.csv", low_memory=False) #low_memory=False is to adjust dtype, whatever that is.
+df = pd.read_csv("Google_Mobility_Report.csv", low_memory=False) #low_memory=False is to adjust dtype, whatever that is.
 #Much easier than Johns Hopkins in that there is one file, so all we need to do is read that csv file into a table.
+
+df.head()
+
+
+# %%
+countries = ['United Arab Emirates', 'United States']
+
+for country in countries:
+    c = df[df['country_region'] == country]
+    plt.plot(c['date'], c['retail_and_recreation_percent_change_from_baseline'], label = country)
+    
+plt.legend
+plt.show
+
+#Google Mobility Data
